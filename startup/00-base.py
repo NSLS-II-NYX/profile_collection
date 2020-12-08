18f39a3c3c1126f837c50ba5d638c70e13549db2
Bruno Martins <brunoseivam@gmail.com>
2020-12-07 21:45:14 -0500

    Disable Progress Bar

--- startup/00-base.py
+++ startup/00-base.py
@@ -4,8 +4,9 @@ import appdirs
 import nslsii
 from bluesky.utils import PersistentDict
 
-nslsii.configure_base(get_ipython().user_ns, 'nyx')
-
+# Base configuration
+#  pbar=False disables progress bar (undesirable in Jupyter)
+nslsii.configure_base(get_ipython().user_ns, 'nyx', pbar=False)
 
 runengine_metadata_dir = appdirs.user_data_dir(appname="bluesky") / Path("runengine-metadata")
 
@@ -14,3 +15,4 @@ RE.md = PersistentDict(runengine_metadata_dir)
 
 RE.md['beamline_name'] = 'NYX'
 RE.md['facility'] = 'NSLS-II'
+

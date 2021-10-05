ce36d055a225bc6279168f95094626368b2ec44c
Jun Aishima <jaishima@bnl.gov>
2021-10-05 14:26:05 -0400

    instantiate from nyxtools
     * make object available in profile collection, use from
       nyxtools

--- startup/50-objects.py
+++ startup/50-objects.py
@@ -1,3 +1,5 @@
 from nyxtools.robot import DensoOphydRobot
+from nyxtools.governor import _make_governors
 
 robot = DensoOphydRobot("XF:19IDC-ES{Rbt:1}", name="robot")
+govs = _make_governors("XF:19IDC-ES", name="govs")

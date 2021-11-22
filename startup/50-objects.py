ce6f8d78ed933669cd9366bbc8c22c94e5adbd62
Jun Aishima <jaishima@bnl.gov>
2021-11-22 14:39:28 -0500

    instantiate flyer and its dependencies
     * detector to be added later

--- startup/50-objects.py
+++ startup/50-objects.py
@@ -1,5 +1,11 @@
 from nyxtools.robot import DensoOphydRobot
 from nyxtools.governor import _make_governors
+from mxtools.zebra import Zebra
+from nyxtools.vector import VectorProgram
+from nyxtools.flyer import NYXFlyer
 
 robot = DensoOphydRobot("XF:19IDC-ES{Rbt:1}", name="robot")
 govs = _make_governors("XF:19IDC-ES", name="govs")
+zebra = Zebra("XF:19IDC-ES{Zeb:1}:", name="zebra")
+vector = VectorProgram("XF:19IDC-ES{Gon:1-Vec}", name="vector")
+flyer = NYXFlyer(vector=vector, zebra=zebra)

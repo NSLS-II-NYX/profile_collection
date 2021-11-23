71775f5605c882d1359d3f09583a0b5ed662dc12
Jun Aishima <jaishima@bnl.gov>
2021-11-23 16:13:53 -0500

    instantiate Pilatus detector, add to flyer

--- startup/50-objects.py
+++ startup/50-objects.py
@@ -2,10 +2,12 @@ from nyxtools.robot import DensoOphydRobot
 from nyxtools.governor import _make_governors
 from mxtools.zebra import Zebra
 from nyxtools.vector import VectorProgram
+from nyxtools.pilatus_detector import PilatusBase
 from nyxtools.flyer import NYXFlyer
 
 robot = DensoOphydRobot("XF:19IDC-ES{Rbt:1}", name="robot")
 govs = _make_governors("XF:19IDC-ES", name="govs")
 zebra = Zebra("XF:19IDC-ES{Zeb:1}:", name="zebra")
+detector = PilatusBase("XF:19IDC-ES{Det:Pil6M}", name="detector")
 vector = VectorProgram("XF:19IDC-ES{Gon:1-Vec}", name="vector")
-flyer = NYXFlyer(vector=vector, zebra=zebra)
+flyer = NYXFlyer(vector=vector, zebra=zebra, detector=detector)
